013c775f30033cc4b136015b1a02f0d4bb2d6db7
BalrajSinghGidda <anonystargamerz@gmail.com>
2026-04-18 23:04:21 +0530

    works on macOS (atleast)

--- flake.nix
+++ flake.nix
@@ -5,30 +5,30 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		nvf.url = "git+https://github.com/notashelf/nvf";
 	};
-	outputs = { self, nixpkgs, nvf, ... }:
-	let
-	  systems = [ "x86_64-linux" "x86_64-darwin" ];
-	in
-	{
-	  packages = nixpkgs.lib.genAttrs systems (system:
-	    let pkgs = import nixpkgs { inherit system; };
-	        nvim = nvf.lib.neovimConfiguration {
-	          inherit pkgs;
-	          modules = [ ./config ];
-	        };
-	    in {
-	      default = nvim;
-	    }
-	  );
-	
-	  apps = nixpkgs.lib.genAttrs systems (system:
-	    let pkg = self.packages.${system}.default;
-	    in {
-	      default = {
-	        type = "app";
-	        program = "${pkg}/bin/nvim";
-	      };
-	    }
-	  );
-	};
+outputs = { self, nixpkgs, nvf, ... }:
+let
+  systems = [ "x86_64-linux" "x86_64-darwin" ];
+in
+{
+  packages = nixpkgs.lib.genAttrs systems (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      default = (nvf.lib.neovimConfiguration {
+        inherit pkgs;
+        modules = [ ./config ];
+      }).neovim;
+    }
+  );
+
+  apps = nixpkgs.lib.genAttrs systems (system:
+    let pkg = self.packages.${system}.default;
+    in {
+      default = {
+        type = "app";
+        program = "${pkg}/bin/nvim";
+      };
+    }
+  );
+};
 }
