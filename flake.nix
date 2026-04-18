b776e26a6f58e0f25fa6cac47895d66b908917ee
BalrajSinghGidda <anonystargamerz@gmail.com>
2026-04-18 22:54:28 +0530

    iuhjo

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		nvf.url = "git+https://github.com/notashelf/nvf";
 	};
-
 	outputs = { self, nixpkgs, nvf, ... }:
 	let
 	  systems = [ "x86_64-linux" "x86_64-darwin" ];
@@ -13,10 +12,21 @@
 	{
 	  packages = nixpkgs.lib.genAttrs systems (system:
 	    let pkgs = import nixpkgs { inherit system; };
+	        nvim = nvf.lib.neovimConfiguration {
+	          inherit pkgs;
+	          modules = [ ./config ];
+	        };
 	    in {
-	      default = nvf.lib.neovimConfiguration {
-	        inherit pkgs;
-	        modules = [ ./config ];
+	      default = nvim;
+	    }
+	  );
+	
+	  apps = nixpkgs.lib.genAttrs systems (system:
+	    let pkg = self.packages.${system}.default;
+	    in {
+	      default = {
+	        type = "app";
+	        program = "${pkg}/bin/nvim";
 	      };
 	    }
 	  );
