e1bff212252fea4b2f7715f7fc45d5932a796b45
BalrajSinghGidda <anonystargamerz@gmail.com>
2026-05-04 14:20:52 +0530

    fixing which key

--- config/default.nix
+++ config/default.nix
@@ -53,131 +53,133 @@
 
     # Custom colorscheme via luaConfigPre
     luaConfigPre = ''
-      -- Disable transparency
-      vim.g.transparent_enabled = false
+            -- Disable transparency
+            vim.g.transparent_enabled = false
 
-require("which-key").setup({
-  plugins = {
-    spelling = true,
-  },
-  window = {
-    border = "rounded",
-  },
-})
+            vim.g.mapleader = " "
 
-      -- Custom colorscheme based on new1.md
-      vim.cmd([[
-        hi clear
-        if exists("syntax_on")
-          syntax reset
-        endif
-        set background=dark
-        let g:colors_name = "custom"
+      local wk = require("which-key")
 
-        " Base colors - explicitly opaque
-        hi Normal guifg=#f4decd guibg=#0f0f0f
-        hi NormalFloat guifg=#f4decd guibg=#0f0f0f
-        hi NormalNC guifg=#f4decd guibg=#0f0f0f
-        hi Visual guifg=#0f0f0f guibg=#f4decd
-        hi Cursor guifg=#0f0f0f guibg=#f4decd
-        hi CursorLine guibg=#1e1f1e
-        hi CursorColumn guibg=#1e1f1e
-        hi LineNr guifg=#3b403c guibg=#0f0f0f
-        hi CursorLineNr guifg=#f4decd guibg=#1e1f1e gui=bold
+      wk.setup({
+        plugins = {
+          spelling = true,
+        },
+        win = {
+          border = "rounded",
+        },
+      })
 
-        " Syntax highlighting
-        hi Comment guifg=#3b403c gui=italic
-        hi Constant guifg=#ebadd6
-        hi String guifg=#a4daa4
-        hi Character guifg=#a4daa4
-        hi Number guifg=#ebadd6
-        hi Boolean guifg=#ebadd6
-        hi Float guifg=#ebadd6
-        hi Identifier guifg=#b0d4e8
-        hi Function guifg=#b0d4e8
-        hi Statement guifg=#ef968f gui=bold
-        hi Conditional guifg=#ef968f
-        hi Repeat guifg=#ef968f
-        hi Label guifg=#f4b585
-        hi Operator guifg=#f4decd
-        hi Keyword guifg=#ef968f
-        hi Exception guifg=#ef968f
-        hi PreProc guifg=#abd4bf
-        hi Include guifg=#abd4bf
-        hi Define guifg=#ebadd6
-        hi Macro guifg=#ebadd6
-        hi PreCondit guifg=#abd4bf
-        hi Type guifg=#f4b585 gui=bold
-        hi StorageClass guifg=#f4b585
-        hi Structure guifg=#f4b585
-        hi Typedef guifg=#f4b585
-        hi Special guifg=#abd4bf
-        hi SpecialChar guifg=#ebadd6
-        hi Tag guifg=#abd4bf
-        hi Delimiter guifg=#f4decd
-        hi SpecialComment guifg=#3b403c gui=italic
-        hi Debug guifg=#ef968f
-        hi Underlined guifg=#b0d4e8 gui=underline
-        hi Ignore guifg=#3b403c
-        hi Error guifg=#ef968f guibg=#1e1f1e
-        hi Todo guifg=#ebadd6 guibg=#1e1f1e gui=bold
+      wk.add({
+        { "<leader>f", group = "Find" },
+        { "<leader>ff", desc = "Files" },
+        { "<leader>fg", desc = "Grep" },
+        { "<leader>fb", desc = "Buffers" },
 
-        " UI elements
-        hi Pmenu guifg=#f4decd guibg=#1e1f1e
-        hi PmenuSel guifg=#0f0f0f guibg=#b0d4e8
-        hi PmenuSbar guibg=#3b403c
-        hi PmenuThumb guibg=#f4decd
-        hi StatusLine guifg=#f4decd guibg=#1e1f1e gui=bold
-        hi StatusLineNC guifg=#3b403c guibg=#1e1f1e
-        hi TabLine guifg=#3b403c guibg=#1e1f1e
-        hi TabLineFill guibg=#1e1f1e
-        hi TabLineSel guifg=#f4decd guibg=#0f0f0f gui=bold
-        hi VertSplit guifg=#3b403c guibg=#0f0f0f
-        hi Folded guifg=#3b403c guibg=#1e1f1e
-        hi FoldColumn guifg=#3b403c guibg=#0f0f0f
-        hi SignColumn guibg=#0f0f0f
-        hi Search guifg=#0f0f0f guibg=#f4b585
-        hi IncSearch guifg=#0f0f0f guibg=#ef968f
-        hi MatchParen guifg=#0f0f0f guibg=#ebadd6 gui=bold
-        hi NonText guifg=#3b403c
-        hi SpecialKey guifg=#3b403c
-        hi Title guifg=#b0d4e8 gui=bold
-        hi Directory guifg=#abd4bf
-        hi ErrorMsg guifg=#ef968f guibg=#0f0f0f
-        hi WarningMsg guifg=#f4b585
-        hi MoreMsg guifg=#a4daa4
-        hi Question guifg=#a4daa4
-        hi WildMenu guifg=#0f0f0f guibg=#b0d4e8
+        { "<leader>g", group = "Git" },
+        { "<leader>t", group = "Terminal" },
+      })
 
-        " Diff colors
-        hi DiffAdd guifg=#a4daa4 guibg=#1e1f1e
-        hi DiffChange guifg=#f4b585 guibg=#1e1f1e
-        hi DiffDelete guifg=#ef968f guibg=#1e1f1e
-        hi DiffText guifg=#b0d4e8 guibg=#3b403c gui=bold
+            -- Custom colorscheme based on new1.md
+            vim.cmd([[
+              hi clear
+              if exists("syntax_on")
+                syntax reset
+              endif
+              set background=dark
+              let g:colors_name = "custom"
 
-        " Git signs
-        hi GitSignsAdd guifg=#a4daa4
-        hi GitSignsChange guifg=#f4b585
-        hi GitSignsDelete guifg=#ef968f
+              " Base colors - explicitly opaque
+              hi Normal guifg=#f4decd guibg=#0f0f0f
+              hi NormalFloat guifg=#f4decd guibg=#0f0f0f
+              hi NormalNC guifg=#f4decd guibg=#0f0f0f
+              hi Visual guifg=#0f0f0f guibg=#f4decd
+              hi Cursor guifg=#0f0f0f guibg=#f4decd
+              hi CursorLine guibg=#1e1f1e
+              hi CursorColumn guibg=#1e1f1e
+              hi LineNr guifg=#3b403c guibg=#0f0f0f
+              hi CursorLineNr guifg=#f4decd guibg=#1e1f1e gui=bold
 
-        " Diagnostics
-        hi DiagnosticError guifg=#ef968f
-        hi DiagnosticWarn guifg=#f4b585
-        hi DiagnosticInfo guifg=#b0d4e8
-        hi DiagnosticHint guifg=#abd4bf
-      ]])
-local wk = require("which-key")
+              " Syntax highlighting
+              hi Comment guifg=#3b403c gui=italic
+              hi Constant guifg=#ebadd6
+              hi String guifg=#a4daa4
+              hi Character guifg=#a4daa4
+              hi Number guifg=#ebadd6
+              hi Boolean guifg=#ebadd6
+              hi Float guifg=#ebadd6
+              hi Identifier guifg=#b0d4e8
+              hi Function guifg=#b0d4e8
+              hi Statement guifg=#ef968f gui=bold
+              hi Conditional guifg=#ef968f
+              hi Repeat guifg=#ef968f
+              hi Label guifg=#f4b585
+              hi Operator guifg=#f4decd
+              hi Keyword guifg=#ef968f
+              hi Exception guifg=#ef968f
+              hi PreProc guifg=#abd4bf
+              hi Include guifg=#abd4bf
+              hi Define guifg=#ebadd6
+              hi Macro guifg=#ebadd6
+              hi PreCondit guifg=#abd4bf
+              hi Type guifg=#f4b585 gui=bold
+              hi StorageClass guifg=#f4b585
+              hi Structure guifg=#f4b585
+              hi Typedef guifg=#f4b585
+              hi Special guifg=#abd4bf
+              hi SpecialChar guifg=#ebadd6
+              hi Tag guifg=#abd4bf
+              hi Delimiter guifg=#f4decd
+              hi SpecialComment guifg=#3b403c gui=italic
+              hi Debug guifg=#ef968f
+              hi Underlined guifg=#b0d4e8 gui=underline
+              hi Ignore guifg=#3b403c
+              hi Error guifg=#ef968f guibg=#1e1f1e
+              hi Todo guifg=#ebadd6 guibg=#1e1f1e gui=bold
 
-wk.register({
-  f = {
-    name = "Find",
-    f = "Files",
-    g = "Grep",
-    b = "Buffers",
-  },
-  g = { name = "Git" },
-  t = { name = "Terminal" },
-}, { prefix = "<leader>" })
+              " UI elements
+              hi Pmenu guifg=#f4decd guibg=#1e1f1e
+              hi PmenuSel guifg=#0f0f0f guibg=#b0d4e8
+              hi PmenuSbar guibg=#3b403c
+              hi PmenuThumb guibg=#f4decd
+              hi StatusLine guifg=#f4decd guibg=#1e1f1e gui=bold
+              hi StatusLineNC guifg=#3b403c guibg=#1e1f1e
+              hi TabLine guifg=#3b403c guibg=#1e1f1e
+              hi TabLineFill guibg=#1e1f1e
+              hi TabLineSel guifg=#f4decd guibg=#0f0f0f gui=bold
+              hi VertSplit guifg=#3b403c guibg=#0f0f0f
+              hi Folded guifg=#3b403c guibg=#1e1f1e
+              hi FoldColumn guifg=#3b403c guibg=#0f0f0f
+              hi SignColumn guibg=#0f0f0f
+              hi Search guifg=#0f0f0f guibg=#f4b585
+              hi IncSearch guifg=#0f0f0f guibg=#ef968f
+              hi MatchParen guifg=#0f0f0f guibg=#ebadd6 gui=bold
+              hi NonText guifg=#3b403c
+              hi SpecialKey guifg=#3b403c
+              hi Title guifg=#b0d4e8 gui=bold
+              hi Directory guifg=#abd4bf
+              hi ErrorMsg guifg=#ef968f guibg=#0f0f0f
+              hi WarningMsg guifg=#f4b585
+              hi MoreMsg guifg=#a4daa4
+              hi Question guifg=#a4daa4
+              hi WildMenu guifg=#0f0f0f guibg=#b0d4e8
+
+              " Diff colors
+              hi DiffAdd guifg=#a4daa4 guibg=#1e1f1e
+              hi DiffChange guifg=#f4b585 guibg=#1e1f1e
+              hi DiffDelete guifg=#ef968f guibg=#1e1f1e
+              hi DiffText guifg=#b0d4e8 guibg=#3b403c gui=bold
+
+              " Git signs
+              hi GitSignsAdd guifg=#a4daa4
+              hi GitSignsChange guifg=#f4b585
+              hi GitSignsDelete guifg=#ef968f
+
+              " Diagnostics
+              hi DiagnosticError guifg=#ef968f
+              hi DiagnosticWarn guifg=#f4b585
+              hi DiagnosticInfo guifg=#b0d4e8
+              hi DiagnosticHint guifg=#abd4bf
+            ]])
     '';
 
     # Editor options
@@ -186,11 +188,11 @@ wk.register({
       tabstop = 4; # Tab displays as 4 spaces
     };
 
-extraPlugins = {
-  which-key = {
-    package = pkgs.vimPlugins.which-key-nvim;
-  };
-};
+    extraPlugins = {
+      which-key = {
+        package = pkgs.vimPlugins.which-key-nvim;
+      };
+    };
 
     # Language support configuration
     languages = {
